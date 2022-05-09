3ff303e82eb768c5519781a295cb4f7442db9e4a
murunwas <cengyweb@gmail.com>
2022-05-09 14:16:01 +0200

    2 files modified, 1 added

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,19 @@
 <script>
 import {paginate,next,back,pageSize,goTo} from "./lib"
+import Pagination from "./lib/Pagination.svelte"
+
+const pages=Array.from({length:$paginate.totalPages}, (v, i) => ((i+1)));
 </script>
-<button on:click={next} disabled={$paginate.disableNext}>next</button>
 
-		{#each Array.from({length:$paginate.totalPages}, (v, i) => ((i+1))) as page}
-		<button on:click={()=>goTo(page)} >{page}</button>
-		{/each}
+    <Pagination 
+    {pages} 
+    currentPage={$paginate.currentPage}
+    on:goto={({detail})=>goTo(detail)}
+    on:next={next}
+    on:back={back}
+    />
 
-
-<button on:click={back}  disabled={$paginate.disableBack}>back</button>
-<select bind:value={$pageSize}>
+<select bind:value={$pageSize} class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
 		{#each Array.from({length: 10}, (v, i) => ((i+1)*10)) as question}
 			<option value={question}>
 				{question}
